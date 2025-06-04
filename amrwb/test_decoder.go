1e73dbbe55544e4cb88564e8d37ac1d7bbbb3c84
Andrey Morozov <amg698@gmail.com>
2025-06-04 12:48:33 +0300

    Fix decoder state cleanup and decode call

--- amrwb/test_decoder.go
+++ amrwb/test_decoder.go
@@ -11,7 +11,7 @@ func TestE_MAIN_init_exit(t *testing.T) {
 	if st == nil {
 		t.Fatal("decoder init returned nil")
 	}
-	decoder.D_MAIN_exit(&st)
+	decoder.D_MAIN_close(&st)
 	if st != nil {
 		t.Error("decoder exit did not set pointer to nil")
 	}
@@ -19,12 +19,12 @@ func TestE_MAIN_init_exit(t *testing.T) {
 
 func TestE_MAIN_decode(t *testing.T) {
 	st := decoder.D_MAIN_init()
-	defer decoder.D_MAIN_exit(&st)
+	defer decoder.D_MAIN_close(&st)
 
 	var serial [common.NB_SERIAL_MAX]int16
 	var output [common.L_FRAME16k]int16
 	serial[0] = 0x3c // mock SID / mode frame
-	decoder.D_MAIN_decode(7, st, serial[:], output[:])
+	decoder.D_MAIN_decode(7, serial[:], output[:], st, 0)
 
 	nonzero := false
 	for _, s := range output {
